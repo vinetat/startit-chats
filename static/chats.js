16d0375331e1ebdfad4494abb49536f8016c895c
vinetat <54443468+vinetat@users.noreply.github.com>
2020-01-10 17:02:37 +0200

    Pievienots noformejums
    styls.css

--- static/chats.js
+++ static/chats.js
@@ -8,7 +8,7 @@ Uzstāda laiku pēc kāda atkārtoti izsaukt šo pašu funkciju
 async function lasiChatu() {
     const atbilde = await fetch('/chats/lasi');
     const datuObjekts = await atbilde.json();
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
     await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
     await lasiChatu();
 }
@@ -59,3 +59,30 @@ ievadesLauks.addEventListener("keyup", function(event) {
     suutiZinju();
   }
 });
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chat");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["messages"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
