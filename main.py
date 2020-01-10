b2b2a28ab57f955fee66363361bab6be57a8f850
vinetat <54443468+vinetat@users.noreply.github.com>
2020-01-10 12:20:34 +0200

    Labojumi
    Labojumi 4datnes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 
 app = Flask('app')
@@ -6,9 +7,9 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-    return "Sveiciens no StartIT! Versija2"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
